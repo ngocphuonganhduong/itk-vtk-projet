9b3652cf81127c369ec5a46f0f7b336743ac06e3
Ngoc Phuong Anh DUONG <ngocphuonganh.duong@epita.fr>
2020-10-13 18:10:51 +0200

    save output

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # coding=utf-8
 import itk
-import vtk
+from render import render
 
 DIR = './'
 FILEPATH = DIR + "BRATS_HG0015_T1C.mha"
+OUTPUT_PATH = DIR + "output.mha"
+
+
+def save_image(image_type, image_data):
+	writer = itk.ImageFileWriter[image_type].New()
+	writer.SetFileName(OUTPUT_PATH)
+	writer.SetInput(image_data)
+	writer.Update()
 
 
 def main():
@@ -19,48 +27,11 @@ def main():
 	reader.Update()
 	
 	# segmentation
+	segmented_image_data = reader.GetOutput()
+	save_image(image_type, segmented_image_data)
 	
-	
-	
-	# convert itk image to vtk image data
-	itk_to_vtk_filter = itk.ImageToVTKImageFilter[image_type].New()
-	itk_to_vtk_filter.SetInput(reader.GetOutput())
-	itk_to_vtk_filter.Update()
-	
-	# create volume from image data
-	volume = vtk.vtkVolume()
-	mapper = vtk.vtkSmartVolumeMapper()
-	mapper.SetInputData(itk_to_vtk_filter.GetOutput())
-	volume.SetMapper(mapper)
-	
-	color_func = vtk.vtkColorTransferFunction()
-	opacity_func = vtk.vtkPiecewiseFunction()
-	property = vtk.vtkVolumeProperty()
-	property.SetColor(color_func)
-	property.SetScalarOpacity(opacity_func)
-	property.SetInterpolationTypeToLinear()
-	
-	volume.SetProperty(property)
-	volume.SetMapper(mapper)
-	
-	# render volume
-	ren = vtk.vtkRenderer()
-	ren_win = vtk.vtkRenderWindow()
-	ren_win.AddRenderer(ren)
-	
-	iren = vtk.vtkRenderWindowInteractor()
-	iren.SetRenderWindow(ren_win)
-	iren.GetInteractorStyle().SetDefaultRenderer(ren)
-	
-	ren_win.SetSize(300, 300)
-	ren_win.Render()
-	
-	ren.AddVolume(volume)
-	ren.ResetCamera()
-	
-	ren_win.Render()
-	iren.Initialize()
-	iren.Start()
+	# render volume and slices
+	render(image_type, segmented_image_data)
 
 
 if __name__ == "__main__":
