21909d08402d75edbfe08074a68d03799c849744
Ngoc Phuong Anh DUONG <ngocphuonganh.duong@epita.fr>
2020-10-15 20:42:54 +0200

    slider: fix axis text display

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 Tumor segmentation and visualisation by Kathialina Va and Ngoc Phuong Anh DUONG.
 
 ## References:
+- Visualisation is inspired by [3D Slicer](https://www.slicer.org/)
+- An example of VTK interactions and observers: [here](https://vtk.org/Wiki/VTK/Examples/Python/Interaction/MouseEventsObserver)
 - An example of Image Slicing: [here](https://vtk.org/gitweb?p=VTK.git;a=blob;f=Examples/ImageProcessing/Python/ImageSlicing.py)
 - An example of Slider 2D widget: [here](https://vtk.org/Wiki/VTK/Examples/Cxx/Widgets/Slider2D)
--- render.py
+++ render.py
@@ -5,11 +5,10 @@ from multiprocessing import Process
 
 
 def render_volume(volume):
-	# render volume
 	ren = vtk.vtkRenderer()
 	ren_win = vtk.vtkRenderWindow()
 	ren_win.AddRenderer(ren)
-	ren_win.SetSize(300, 300)
+	ren_win.SetSize(480, 480)
 	
 	iren = vtk.vtkRenderWindowInteractor()
 	iren.SetRenderWindow(ren_win)
@@ -24,7 +23,7 @@ def render_volume(volume):
 
 
 class CustomRender:
-	axes_name = ["axial - x", "coronal - y", "sagittal - z"]
+	axes_name = ["Axial - x", "Coronal - y", "Sagittal - z"]
 	
 	def __set_up_filter(self, reader):
 		self.filter = vtk.vtkImageReslice()
@@ -35,7 +34,7 @@ class CustomRender:
 	
 	def __set_up_drawing_actor(self):
 		table = vtk.vtkLookupTable()
-		table.SetRange(0, 2000)  # image intensity range
+		table.SetRange(0, 255)
 		table.SetValueRange(0.0, 1.0)  # from black to white
 		table.SetSaturationRange(0.0, 0.0)  # no color saturation
 		table.SetRampToLinear()
@@ -53,13 +52,14 @@ class CustomRender:
 	def __set_up_renderer(self):
 		self.ren = vtk.vtkRenderer()
 		self.ren.AddActor(self.actor)
+		self.ren.ResetCamera()
 		
 		self.win = vtk.vtkRenderWindow()
 		self.win.AddRenderer(self.ren)
+		self.win.SetSize(480, 480)
 		
 		self.style = vtk.vtkInteractorStyleImage()
 		self.style.AddObserver("RightButtonReleaseEvent", self.switch_axis_call_back)
-		
 		self.iren = vtk.vtkRenderWindowInteractor()
 		self.iren.SetInteractorStyle(self.style)
 		self.win.SetInteractor(self.iren)
@@ -70,26 +70,23 @@ class CustomRender:
 		self.slider_rep.SetMinimumValue(minv)
 		self.slider_rep.SetMaximumValue(maxv)
 		self.slider_rep.SetValue((maxv - minv) / 2.0)
-		self.slider_rep.GetSliderProperty().SetColor(0, 0, 1)
-		self.slider_rep.GetTitleProperty().SetColor(0, 0, 1)
-		self.slider_rep.GetLabelProperty().SetColor(0, 0, 1)
-		self.slider_rep.GetSelectedProperty().SetColor(1, 0, 0)
-		self.slider_rep.GetCapProperty().SetColor(1, 0, 0)
-		self.slider_rep.GetTubeProperty().SetColor(1, 0, 0)
-		self.slider_rep.GetTubeProperty().SetColor(1, 0, 0)
-		self.slider_rep.GetTubeProperty().SetColor(1, 0, 0)
-		self.slider_rep.GetPoint1Coordinate().SetCoordinateSystemToDisplay()
-		self.slider_rep.GetPoint1Coordinate().SetValue(20, 40)
-		self.slider_rep.GetPoint2Coordinate().SetCoordinateSystemToDisplay()
-		self.slider_rep.GetPoint2Coordinate().SetValue(300, 40)
 		self.slider_rep.SetTitleText(self.axes_name[self.axis])
+		
+		self.slider_rep.GetSliderProperty().SetColor(1, 1, 0.1)
+		self.slider_rep.GetSelectedProperty().SetColor(0, 1, 0)
+		self.slider_rep.GetCapProperty().SetColor(0, 0.2, 0.5)
+		self.slider_rep.GetTubeProperty().SetColor(0, 0.1, 0.4)
+		self.slider_rep.GetPoint1Coordinate().SetCoordinateSystemToDisplay()
+		self.slider_rep.GetPoint1Coordinate().SetValue(60, 100)
+		self.slider_rep.GetPoint2Coordinate().SetCoordinateSystemToDisplay()
+		self.slider_rep.GetPoint2Coordinate().SetValue(260, 100)
 		
 		self.slider_wid = vtk.vtkSliderWidget()
 		self.slider_wid.SetInteractor(self.iren)
 		self.slider_wid.SetRepresentation(self.slider_rep)
 		self.slider_wid.SetAnimationModeToAnimate()
 		self.slider_wid.EnabledOn()
-		self.slider_wid.AddObserver("InteractionEvent", self.slider_call_back)
+		self.slider_wid.AddObserver(vtk.vtkCommand.InteractionEvent, self.slider_call_back)
 	
 	def __init__(self, reader, default_axis=0):
 		min_x, max_x, min_y, max_y, min_z, max_z = reader.GetExecutive().GetWholeExtent(reader.GetOutputInformation(0))
@@ -131,12 +128,12 @@ class CustomRender:
 		self.filter.Update()
 		matrix = self.filter.GetResliceAxes()
 		matrix.SetElement(2 - self.axis, 3, self.origin[self.axis] + ratio * self.size[self.axis])
-		self.win.Render()
 	
 	def switch_axis_call_back(self, obj, event):
 		self.axis = (self.axis + 1) % 3
 		self.filter.SetResliceAxes(self.axes[self.axis])
 		self.slider_rep.SetValue((self.slider_rep.GetMaximumValue() - self.slider_rep.GetMinimumValue()) / 2.0)
+		self.slider_rep.SetTitleText(self.axes_name[self.axis])
 		self.win.Render()
 
 
