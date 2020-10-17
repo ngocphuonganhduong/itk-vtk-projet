e6f82f8fad0c984e8934a9e335707bbb7d732a49
kathialina.va <kathialina.va@epita.fr>
2020-10-17 22:44:31 +0200

    [ITK][SEGMENT] Segment brain tumor

--- main.py
+++ main.py
@@ -18,31 +18,39 @@ def main():
 	# ITK version should < 5.1 to use ImageToVTKImageFilter
 	print("ITK version: ", itk.Version.GetITKVersion())
 	
-	pixel_type = itk.ctype('short')
+	input_pixel_type = itk.ctype('float')
+	output_pixel_type = itk.ctype('short')
 	dim = 3
-	image_type = itk.Image[pixel_type, dim]
+	output_image_type = itk.Image[output_pixel_type, dim]
+	input_image_type = itk.Image[input_pixel_type, dim]
 	
-	reader = itk.ImageFileReader[image_type].New()
+	reader = itk.ImageFileReader[input_image_type].New()
 	reader.SetFileName(FILEPATH)
 	reader.Update()
 	
 	# segmentation
-	segment = itk.ConnectedThresholdImageFilter[image_type, image_type].New()
-	segment.SetInput(reader.GetOutput())
-	segment.SetSeed([100, 100, 100])
-	segment.SetUpper(600)
-	segment.SetLower(100)
+	prefilter = itk.GradientAnisotropicDiffusionImageFilter[input_image_type, input_image_type].New()
+	prefilter.SetInput(reader.GetOutput())
+	prefilter.SetNumberOfIterations(20)
+	prefilter.SetConductanceParameter(3)
+	prefilter.SetTimeStep(0.04)
 
-	rescaler = itk.RescaleIntensityImageFilter[image_type, image_type].New()
+	segment = itk.ConnectedThresholdImageFilter[input_image_type, input_image_type].New()
+	segment.SetInput(prefilter.GetOutput())
+	segment.SetSeed([75, 80, 100])
+	segment.SetUpper(1500)
+	segment.SetLower(900)
+
+	rescaler = itk.RescaleIntensityImageFilter[input_image_type, output_image_type].New()
 	rescaler.SetInput(segment.GetOutput())
 	rescaler.SetOutputMinimum(0)
 	rescaler.SetOutputMaximum(255)
 	
 	segmented_image_data = rescaler.GetOutput()
-	save_image(image_type, segmented_image_data)
+	save_image(output_image_type, segmented_image_data)
 	
 	# render volume and slices
-	render(image_type, segmented_image_data)
+	render(output_image_type, segmented_image_data)
 
 
 if __name__ == "__main__":
